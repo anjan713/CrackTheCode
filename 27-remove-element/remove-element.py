b48b80b918586e6f0389bb3f55da541cd6178e25
Anjan Narla <40123185+anjan713@users.noreply.github.com>
2025-06-11 23:49:21 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (11.14%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        insert_pos = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[insert_pos] = nums[i]
+                insert_pos += 1
+        return insert_pos
